35d653c083d708d59b4f02dba807add0810ebec3
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-10-27 14:48:23 +0100

    Created UML class diagram for TocTocV2

---
+++ src/main/java/com/toctoc/toctoc2/TocToc2Application.java
@@ -0,0 +1,13 @@
+package com.toctoc.toctoc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TocToc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TocToc2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/toctoc/toctoc2/TocToc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toctoc.toctoc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TocToc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
