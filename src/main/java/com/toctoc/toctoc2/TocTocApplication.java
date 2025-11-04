e92601ee005b8f1f6291d82a94ddfaefee18144c
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-11-04 15:05:32 +0100

    Added necessary annotations for Jpa and SpringBoot

--- src/main/java/com/toctoc/toctoc2/TocTocApplication.java
+++ src/main/java/com/toctoc/toctoc2/TocTocApplication.java
@@ -2,12 +2,13 @@ package com.toctoc.toctoc2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TocToc2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(TocToc2Application.class, args);
     }
-
 }
